9a2f9dcdf550869c158833211e7eec6cb0eb59ca
ErinFelts <136392656+ErinFelts@users.noreply.github.com>
2023-07-23 13:33:55 -0400

    adding comments

--- alltogethercode.c
+++ alltogethercode.c
@@ -9,25 +9,25 @@ int numColumns;
 //easy= 8*8 contains 10 potholes
 //medium=9*9 contains 30 potholes
 //hard=10*10 contains  50 potholes
-int numPot;
+int numPot; // number of potholes
 int *pots; //array of positions of potholes
-char diff[100];
+char diff[100]; //used to change input to uppercase
 int **map;
 bool checking=true;
-int **mapUpdated;
+int **mapUpdated; // map that is updated after each guess
 int map2Val=0;
- bool already_guessed = false;
+ bool already_guessed = false; //if value has been guessed
 
 
 void mapMake()
 {
 while(true)
 {
-printf("\n	What difficulty would you like to play?\n Choose from: easy, medium, or hard.\n");
+printf("\n	What difficulty would you like to play?\n Choose from: easy, medium, or hard.\n"); //difficulties have preset values
 scanf("%s",diff);
 	for(int i=0; i<strlen(diff);i++)
 {
-		diff[i]=toupper(diff[i]);
+		diff[i]=toupper(diff[i]); // makes it so that any combination of capital/ not capital is accepted
 }
 	if(strcmp(diff, "EASY")==0)
 {
@@ -60,20 +60,20 @@ printf(" Which is not an accepted difficulty. Please try again, and choose from 
 }
 
 }
-map = (int**)malloc(numColumns * sizeof(int*));
-mapUpdated = (int**)malloc(numColumns * sizeof(int*));
+map = (int**)malloc(numColumns * sizeof(int*)); // map size of rows and columns dependent on difficulty mode
+mapUpdated = (int**)malloc(numColumns * sizeof(int*)); // this helps with the issue of dynamic/ constant matrices
 
-for (int i = 0; i < numColumns; i++)
+for (int i = 0; i < numColumns; i++) //initializes
 	{
     map[i] = (int*)malloc(numRows * sizeof(int));
-    mapUpdated[i] = (int*)malloc(numRows * sizeof(int));
+    mapUpdated[i] = (int*)malloc(numRows * sizeof(int)); 
 	}
 pots=(int*)malloc(numPot*sizeof(int));
 //Great! difficulty chose, #potholes created, area made, and we know the values of rows and columns now. We can move on with these values.
 //first for loop makes an array of positions of potholes
 for(int i=0;i<numPot;i++)
 {
-	pots[i]=rand()%(numRows*numColumns)+1; //randomizes postions of potholes, witin the values of the map.
+	pots[i]=rand()%(numRows*numColumns)+1; //randomizes postions of potholes, witin the values of the map. based on difficulty
 	for(int j=0; j<i; j++)
 	{
 		//printf("%d",pots[i]);
@@ -88,13 +88,13 @@ for(int i=0;i<numColumns;i++)
 {
 	for(int j=0;j<numRows;j++)
 	{
-		spotVal++; // holds "value" of spot, 1 up to area value.
+		spotVal++; // holds "value" of spot, 1 up to area value. Helps traverse each spot of the map.
 		for(int k=0;k<numPot;k++)
 		{
 		
-			if(pots[k]==spotVal)
+			if(pots[k]==spotVal) // if the space of the map we are in is one of the pothole vlues stored in pots, that is a pothole in map.
 			{
-				map[j][i]=1;
+				map[j][i]=1; //1 is the value we assign if it is a pothole.
 				mapUpdated[j][i]=1;
 				break;
 			}
@@ -102,17 +102,17 @@ for(int i=0;i<numColumns;i++)
 			{
 				map[j][i]=9;
 				mapUpdated[j][i]=9;
+		//We fill the map with 9 if it is a space without potholes, as the number of potholes around
+		// a space can be any number 0-8, but never 9. There are only 8 blocks in the radius of 1 block.
 			}
 			
 		}
-		//printf("%d",mapUpdated[j][i]);
-			//printf(" ");
 	}
 }
 }
 //##########################################################################################
 
-void check_guess_update_map(int guess) {
+void check_guess_update_map(int guess) { // checks guess and updates map to show values.
     int spotVal2=0;
 	bool check;
 	
@@ -125,15 +125,19 @@ for(int i=0;i<numColumns;i++)
 		{
 			if(map[j][i]==1)
 			{
-				checking=false;
-				already_guessed = false;				// false means you hit a pothole.
+				checking=false; // false means you hit a pothole
+				already_guessed = false;				// false here resets already_guessed
 				return;
 			}
 			else if(mapUpdated[j][i]!=9)
+			{//means, if not a pothole, and the updated map at that point hasn't been set to value of radius potholes (can't be 9)
+				//then you have already updated that spot
 				already_guessed = true;
+			}
 			else
-				already_guessed=false;
+				already_guessed=false; //otherwise you haven't and it is a safe spot
 			int minRow;
+			//below if statements determine the radius spaces around the spot you checked
 			    if(j - 1 >= 0) 
 					minRow =j-1;
 				else
@@ -153,13 +157,14 @@ for(int i=0;i<numColumns;i++)
 					maxCol =i + 1; 
 				else
 					maxCol =numColumns - 1;
-				
+				//now that we know what spots we can check(not out of bounds) we can add the values at those spots
+			// so, if bomb, then +1, if ==9, then no bomb and we don't add
                 for (int row = minRow; row <= maxRow; row++)
 				{
                     for (int col = minCol; col <= maxCol; col++)
 					{
                         if (row == j && col == i)
-                            continue; // skip the center spot
+                            continue; // skip the center spot where we are checking
 					if(map[row][col]!=9)
                         map2Val += map[row][col];
                     }
@@ -167,12 +172,12 @@ for(int i=0;i<numColumns;i++)
                     
                 
 			//map2Val=mapUpdated[j-1][i-1]+mapUpdated[j-1][i]+mapUpdated[j-1][i+1]+mapUpdated[j][i-1]+mapUpdated[j][i+1]+mapUpdated[j+1][i-1]+mapUpdated[j+1][i]+mapUpdated[j+1][i+1];
-			mapUpdated[j][i]=map2Val;
+			mapUpdated[j][i]=map2Val; // this sets that spot on the updated map to the value of potholes around it.
 		}
 				
 		
 	}
-    checking=true;
+    checking=true; //if you got this far, yo uhit a safe space
 }
 }
 //##########################################################################################
@@ -194,6 +199,9 @@ void print_map(int guess)
 			numbr++;
             //if (numbr==guess)
 				//printf(". ");
+			//next line was hard to determine. if original map and updated map are both 1, spot not discovered and place x. 
+			//if both maps are 9, spot also not discovered so place x. If both not true, spot discovered and value would have been
+			//saved in updated map, so print that 
 			if((mapUpdated[j][i]==1&&map[j][i]==1)||(mapUpdated[j][i]==9&&map[j][i]==9 ))
                 printf("X "); 
 			else 
